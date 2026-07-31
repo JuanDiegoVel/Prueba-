51a5b9edd2f305034f64e5ad304b2032522e5412
JuanDiegoVel <juandiegovelascoquintero4@gmail.com>
2026-07-31 17:29:13 -0500

    Configura estructura base del proyecto con Spring Boot

---
+++ src/main/java/com/example/crudproductos/CrudProductosApplication.java
@@ -0,0 +1,12 @@
+package com.example.crudproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudProductosApplication.class, args);
+    }
+}
